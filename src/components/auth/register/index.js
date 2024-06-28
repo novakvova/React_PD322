32fad23846e0f00cfca4237b994f41d1da06de81
novakvova <novakvova@gmail.com>
2024-06-28 16:14:10 +0300

    Add file Input

--- src/components/auth/register/index.js
+++ src/components/auth/register/index.js
@@ -1,19 +1,62 @@
-import TextInput from "../../common/input";
+import TextInput from "../../common/textInput";
+import {useState} from "react";
+import FileInput from "../../common/fileInput";
 
 const RegisterPage = () => {
 
+    const initValue = {
+        firstName: "Вова",
+        lastName: "",
+        phone: "",
+        image: null
+    };
+
+    const [data, setData] = useState(initValue);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Submit form ");
+        console.log("Submit form ", data);
     }
+
+    const onChangeHandler = (e) => {
+        // console.log("onChange", e.target);
+        // console.log("onChange", e.target.name);
+        // console.log("onChange", e.target.value);
+        setData({...data, [e.target.name]: e.target.value});
+    }
+
+    const onChangeFileHandler = (e) => {
+        console.log("onChange", e.target.files);
+        const file = e.target.files[0];
+        if (file) {
+            setData({...data, [e.target.name]: file});
+        }
+        else {
+            setData({...data, [e.target.name]: null});
+            alert("Оберіть фото");
+        }
+    }
+
     return (
         <>
             <h1 className={"text-center"}>Реєстрація</h1>
             <form onSubmit={handleSubmit} className={"col-md-6 offset-md-3"}>
+                <TextInput label={"Прізвище"} field={"lastName"} type={"text"}
+                           value={data.lastName}
+                           onChange={onChangeHandler}/>
 
-                <TextInput label={"Прізвище"} field={"firstName"} type={"text"} />
-                <TextInput label={"Ім'я"} field={"name"} type={"text"} />
-                <TextInput label={"Телефон"} field={"phone"} type={"text"} />
+                <TextInput label={"Ім'я"} field={"firstName"} type={"text"}
+                           value={data.firstName}
+                           onChange={onChangeHandler}/>
+
+                <TextInput label={"Телефон"} field={"phone"} type={"text"}
+                           value={data.phone}
+                           onChange={onChangeHandler}/>
+
+                <FileInput label={"Фото"} field={"image"}
+                           value={data.image}
+                           onChange={onChangeFileHandler}/>
+
                 <button type="submit" className="btn btn-primary">Реєструватися</button>
 
             </form>
--- src/components/common/fileInput/index.js
+++ src/components/common/fileInput/index.js
@@ -1,18 +1,29 @@
-const TextInput = ({ label, field, type, value, onChange }) => {
+import userImage from "../../../assets/images/user.jpg";
+
+const FileInput = ({ label, field, value, onChange }) => {
+    const img = value == null ? userImage : URL.createObjectURL(value);
     return (
       <>
           <div className="mb-3">
-              <label htmlFor={field} className="form-label">{label}</label>
-              <input type={type}
-                     className="form-control"
-                     id={field}
-                     name={field}
-                     value={value}
-                     onChange={onChange}
-                     aria-describedby="emailHelp"/>
+              <div className="row">
+                  <div className="col-md-3">
+                      <img src={img} alt="" className={"img-fluid"}/>
+                  </div>
+                  <div className="col-md-9">
+                      <label htmlFor={field} className="form-label">{label}</label>
+                      <input type="file"
+                             className="form-control"
+                             id={field}
+                             name={field}
+                             onChange={onChange}
+                             aria-describedby="emailHelp"/>
+                  </div>
+              </div>
+
           </div>
+
       </>
     );
 }
 
-export default TextInput;
+export default FileInput;
